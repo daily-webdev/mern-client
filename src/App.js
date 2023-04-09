c743b73e01e9f696b7669fa29d4deb193aab5a51
shinrin <taublermarcin@gmail.com>
2023-04-09 16:24:36 +0200

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useState } from "react";
 
 const TestComp = () => {
+  const [state, handleState] = useState("nothing");
+  const handleClick = () => {
+    fetch("http://localhost:9000/api")
+      .then((res) => {
+        return res.text();
+      })
+      .then((res) => {
+        const data = res;
+        handleState(data);
+      });
+  };
+
   return (
     <>
       <h1>TEST</h1>
-      <h2>asdsd</h2>
+      <button onClick={handleClick}>get data</button>
+      <div>{state}</div>
     </>
   );
 };
